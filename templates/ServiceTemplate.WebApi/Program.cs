9b0265ca352a1b12fa3f5644e3b3d7aac9d3bd90
sapin <sapin@bfs-it.ru>
2022-02-02 10:33:48 +0300

    Add CORS and logging through Serilog

--- templates/ServiceTemplate.WebApi/Program.cs
+++ templates/ServiceTemplate.WebApi/Program.cs
@@ -1,14 +1,31 @@
 #pragma warning disable SA1516
 
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Formatting.Compact;
 using ServiceTemplate.Application;
 using ServiceTemplate.Model;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+    options.AddDefaultPolicy(corsPolicyBuilder =>
+    {
+        corsPolicyBuilder.WithOrigins("*")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    }));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program));
+builder.Logging.AddSerilog();
+builder.WebHost.UseSerilog((_, configuration) =>
+{
+    configuration
+        .MinimumLevel.Information()
+        .Enrich.FromLogContext()
+        .WriteTo.Console(new RenderedCompactJsonFormatter());
+});
 
 builder.Services.AddDbContext<ServiceDbContext>(options =>
 {
@@ -18,11 +35,16 @@ builder.Services.AddDbContext<ServiceDbContext>(options =>
 builder.Services.AddScoped<IExampleModelRepository, ExampleModelRepository>();
 
 var app = builder.Build();
+
+app.UseCors();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSerilogRequestLogging();
 
 app.MapControllers();
 app.Run();
